a7c42df113aa91e97821a517f8fe22c7a1c2298a
DiscHacker <135727788+DiscHacker@users.noreply.github.com>
2024-09-28 23:44:35 +0700

    Update gems.py

--- src/gems.py
+++ src/gems.py
@@ -1,8 +1,7 @@
-# these values are derived from large benchmarks
 AVERAGE_SCORES = [32.7, 57.5, 74, 86]
 AVERAGE_NET_GEM_PROFITS = [2.8, 1, -0.8, -2.6]
 
-def gem_value(gem_count: int):
+def get_gem_value(gem_count: int):
     value = 0
     for swap_count in range(1, 4):
         gem_threshold = swap_count * 3
@@ -11,3 +10,10 @@ def gem_value(gem_count: int):
         elif gem_count > gem_threshold - 3:
             value += ((AVERAGE_SCORES[swap_count] - AVERAGE_SCORES[swap_count - 1]) / 3) * (gem_count % 3)
     return round(value, 1)
+
+GEM_VALUE_LOOKUP = [get_gem_value(i) for i in range(11)]
+
+def gem_value(gem_count: int):
+    if gem_count >= len(GEM_VALUE_LOOKUP):
+        return GEM_VALUE_LOOKUP[-1]
+    return GEM_VALUE_LOOKUP[gem_count]
